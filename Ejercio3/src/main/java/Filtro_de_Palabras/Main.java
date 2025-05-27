3c43f10d3c726d35d76d422f1cf13f37164b90a6
diegospapus21 <diegoghc7002@>
2025-05-27 10:59:37 -0600

    subiendo el ejersicio3

--- Ejercio3/src/main/java/Filtro_de_Palabras/Main.java
+++ Ejercio3/src/main/java/Filtro_de_Palabras/Main.java
@@ -1,12 +1,10 @@
-package Control_de_Asistencia;
-
+package Filtro_de_Palabras;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ControlAsistencia demo = new ControlAsistencia();
 
-        System.out.println("HashSet");
-        demo.ControlHashSet();
-        }
+        System.out.println("hola mundo");
+            
     }
+}
--- Ejersicisio2/src/main/java/Control_de_Asistencia/ControlAsistencia.java
+++ Ejersicisio2/src/main/java/Control_de_Asistencia/ControlAsistencia.java
@@ -1,4 +1,22 @@
 package Control_de_Asistencia;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ControlAsistencia {
+    public void ControlHashSet() {
+        Set<String> conjunto = new HashSet<>();
+
+        conjunto.add("20230048");
+        conjunto.add("20234440");
+        conjunto.add("20230248");
+        conjunto.add("20234450");
+        conjunto.add("20230048");
+
+        System.out.println("La lista del Control de Asistencia: " + conjunto);
+
+        System.out.println("Este codigo ya esta registrado: " + conjunto.contains(20230048));
+
+
+    }
 }
